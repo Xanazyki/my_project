f7f9e48bdaa0229e755e7c98a1c741b055dbf32d
Mikle Shmatko <shmatko.mixail@mail.ru>
2025-10-04 14:37:36 +0300

    Выполнил задание 3. Слова длиннее 3х символов

--- laba3/1.py
+++ laba3/1.py
@@ -3,8 +3,11 @@ kvadrat = [x**2 for x in range(1, 11)]
 print(kvadrat)
 
 
-#2 все четные числа
+#2. все четные числа
 chet = [x for x in range(1, 20) if x % 2 == 0]
 print(chet)
 
-#3
+#3. Все слова длиннее трех символов
+words = ["python", "Java", "c++", "Rust", "go"]
+result = [word.upper() for word in words if len(word) > 3]
+print(result)
