1bbe76fe41ac61882a3e6e36afe2e08167830f47
Mikle Shmatko <shmatko.mixail@mail.ru>
2025-10-04 14:54:11 +0300

    Выполнил задание 5. Генеработ Финаччи

--- laba3/1.py
+++ laba3/1.py
@@ -7,10 +7,12 @@ print(kvadrat)
 chet = [x for x in range(1, 20) if x % 2 == 0]
 print(chet)
 
+
 #3. Все слова длиннее трех символов
 words = ["python", "Java", "c++", "Rust", "go"]
 result = [word.upper() for word in words if len(word) > 3]
 print(result)
+
 
 #4. Класс-итератор, обратное возвращение
 class Countdown:
@@ -28,7 +30,21 @@ class Countdown:
         self.current -= 1
         return value
 
-n = int(input("Введите число n"))
+n = int(input("Введите число n: "))
 for x in Countdown(n):
     print(x)
 
+
+#5. 
+def fibonacci(n):
+    a, b = 0, 1
+    count = 0
+    while count < n:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+n = int(input("Введите число n: "))
+print(f"Первые {n} чисел Фибоначчи:")
+for num in fibonacci(n):
+    print(num)
